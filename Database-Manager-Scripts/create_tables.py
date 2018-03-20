81a8c4c55ab382c591da83dfb7bf908afbc9b2a8
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-03-20 09:02:39 +0800

    Modify Db_Manager

--- Database-Manager-Scripts/create_tables.py
+++ Database-Manager-Scripts/create_tables.py
@@ -7,7 +7,7 @@ db_pwd = "0000"
 db_name = "erp_system_manager"
 db = pymysql.connect(db_ip,db_usr,db_pwd,db_name);
 cursor = db.cursor()
-tmp_sql = dbcmd.create_account_info_table;
+tmp_sql = dbcmd.create_table_account_info;
 cursor.execute(tmp_sql)
 print("created table successfull.")
 # disconnect from server
--- Database-Manager-Scripts/dbcmd.py
+++ Database-Manager-Scripts/dbcmd.py
@@ -1,6 +1,6 @@
-create_account_info_table = """CREATE TABLE `account_info` (
+create_table_user_info = """CREATE TABLE `account_info` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `account` char(20) NOT NULL,
+  `usr_name` char(20) NOT NULL,
   `pwd_md5` char(40) NOT NULL,
   PRIMARY KEY (`id`)
 );"""
