aed4872d595e5dc5b34f0ad679ed23712c30ea43
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-04-08 14:08:25 +0800

    main

--- ERP-Simulator-Server-End/main.py
+++ ERP-Simulator-Server-End/main.py
@@ -6,6 +6,6 @@
 import ServerSystem
 import sys
 if __name__ == '__main__':
-    erp_server = ServerSystem.ServerSystem(sys.argv[1],int(sys.argv[2]))
-    #erp_server = ServerSystem.ServerSystem('127.0.0.1',8828)
+    #erp_server = ServerSystem.ServerSystem(sys.argv[1],int(sys.argv[2]))
+    erp_server = ServerSystem.ServerSystem('127.0.0.1',8828)
     erp_server.launch()
