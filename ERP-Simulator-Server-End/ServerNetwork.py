9a3d4bbb8074d876bbfc8847362146b3abf85456
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-04-08 08:58:21 +0800

    commit for checkout branch

--- ERP-Simulator-Server-End/ServerNetwork.py
+++ ERP-Simulator-Server-End/ServerNetwork.py
@@ -19,19 +19,27 @@ class ServerSocket(object):
         self.__ref_ser_manager = _s_manager
 
     def launch_server(self):
+        print('System Start Listening')
         self.__socket.listen(self.__max_connect_num)
         while True:
             client_socket_info = self.__socket.accept()
-            address_thread = threading.Thread(target=self.__address_request, args=client_socket_info)
+            address_thread = threading.Thread(target=self.__address_request, args=(client_socket_info,))
             address_thread.start()
 
     def __address_request(self, _c_socket_info):
         # Receive Client Message
         client_socket = _c_socket_info[0]
+        print('Address Request :'+ str(client_socket))
+        counter = 1;
         while True:
+            print(counter)
+            counter = counter + 1
             data = client_socket.recv(1024)
+            print(data.decode('utf-8'))
             if not data:
+                print('break') 
                 break
+        print('Client Message Received')
         client_request = Message.Request(data.decode('utf-8'))
         server_reply = self.__ref_ser_manager.address_request(client_request)
         client_socket.send(server_reply.get_message().encode('utf-8'))
--- ERP-Simulator-Server-End/testClient.py
+++ ERP-Simulator-Server-End/testClient.py
@@ -6,22 +6,14 @@ import socket
 import threading
 
 
-def test(p):
-    print("Thread : " + str(p))
-    for data in range(1):
-        # 发送数据:
-        s.send(('thread' + str(p) + ' : ' + str(data)).encode('utf-8'))
-        print("Client Thread " + str(p)+s.recv(1024).decode('utf-8'))
-
+def test():
+    s.send(('login:'+'myuser'+' '+'123').encode('utf-8'))
+    print("Client Receive : "+s.recv(1024).decode('utf-8'))
     s.close()
 
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # 建立连接:
     s.connect(('127.0.0.1', 8828))
-    # 接收欢迎消息:
-    for i in range(1):
-        print("aa")
-        t = threading.Thread(target=test, args=(str(i)))
-        t.start()
+    t = threading.Thread(target=test, args=())
+    t.start()
