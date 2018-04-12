4d0b470ba04d368376148c4443d828be42df10f2
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-04-12 11:23:26 +0800

    Fix Conflict Bug

--- ERP-Simulator-Server-End/testClient.py
+++ ERP-Simulator-Server-End/testClient.py
@@ -7,7 +7,6 @@ import threading
 
 
 def test():
-<<<<<<< HEAD
     #s.send(('login:'+'cjw'+' '+'cjwcjw').encode('utf-8'))
     #s.send(('logout:'+'4').encode('utf-8'))
     s.sendall(('login:'+'cjw'+' '+'cjwcjw').encode('utf-8'))
@@ -15,22 +14,12 @@ def test():
     data = s.recv(1024).decode('utf-8')
     print(data)  
     print("end")
-=======
-    # s.send(('login:'+'sabrehawk'+' '+'wzqcd753951').encode('utf-8'))
-    # s.send(('logout:'+'4').encode('utf-8'))
-    s.send(('register:'+'zsm'+' '+'110first').encode('utf-8'))
-    print("Client Receive : "+s.recv(1024).decode('utf-8'))
->>>>>>> tmp
     s.close()
 
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-<<<<<<< HEAD
-    s.connect(('140.82.1.250', 8828))
-=======
-    s.connect(('192.168.43.94', 8828))
->>>>>>> tmp
+    s.connect(('127.0.0.1', 8828))
     t = threading.Thread(target=test, args=())
     t.start()
     
